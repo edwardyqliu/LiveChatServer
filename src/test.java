31c826af1734d040440905149533da25b4808105
Edward Liu <edliu@sas.upenn.edu>
2024-05-17 16:32:51 -0400

    again

--- src/test.java
+++ src/test.java
@@ -1,13 +1,16 @@
+import java.util.Map;
+
 public class test {
     class Test {
-        public void main(String[] args) {
-            String script = System.getenv("SCRIPTNAME");
-            if (script != null) {
-                // BAD: The script to be executed is controlled by the user.
-                Runtime.getRuntime().exec(script);
-            } else {
-                main(null);
-            }
-        }
+        public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    String attr = request.getParameter("attribute");
+    String value = request.getParameter("value");
+
+    Map<String, String> env = processBuilder.environment();
+    // BAD: attr and value are tainted and being added to the environment
+    env.put(attr, value);
+
+    processBuilder.start();
+}
     }
 }
